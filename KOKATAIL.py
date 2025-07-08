cc8953da9d2627479bc5fce1fcf00c613d507f47
M.Yoda <c0a240200e@edu.teu.ac.jp>
2025-07-08 18:05:11 +0900

    逃げる機能確認

--- KOKATAIL.py
+++ KOKATAIL.py
@@ -82,7 +82,6 @@ class Escape:
 
         if self.last_result is None:
             return
-        screen.fill(BLACK)
         if self.last_result:
             msg = font.render("逃げた", True, (0, 255, 0))
             msg_rect = msg.get_rect()
